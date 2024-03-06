517ba1d188520d8f1c07c3b62435cea9cbd3515c
Ilkham Khabibullin <mailbox.ilham@gmail.com>
2024-03-06 07:23:16 +0300

    - searching over entities supported - raw interface for creating enitities - some css

--- src/components/admin_components.rs
+++ src/components/admin_components.rs
@@ -3,11 +3,9 @@ use leptos::html::Div;
 use leptos_router::*;
 use log::error;
 use std::collections::BTreeMap;
-use std::future::Future;
+use std::collections::BTreeSet;
 use serde_json::Value;
-use leptos_router::Redirect;
 
-use log::info;
 use leptos::*;
 
 use crate::api::auth::MeError;
@@ -119,6 +117,10 @@ pub fn Sidebar() -> impl IntoView {
                         <li>Users</li>
                     </A>
 
+                    <A href="basics">
+                        <li>Basic users</li>
+                    </A>
+
                     <A href="moderators">
                         <li>Moderators</li>
                     </A>
@@ -157,44 +159,56 @@ pub fn Main() -> impl IntoView {
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Pagination(pub usize, pub usize);
 
-pub type VecOfMaps = Vec<BTreeMap<String, Value>>;
-
-pub type Count = String;
-
-type FetcherResponse = leptos::error::Result<(VecOfMaps, Count)>;
-
-type Fetcher = Box<dyn Fn(Pagination, VecOfMaps) -> std::pin::Pin<Box<dyn Future<Output = FetcherResponse>>>>;
-
-fn force_boxed<T>(f: fn(Pagination, VecOfMaps) -> T) -> Fetcher where T: Future<Output = FetcherResponse> + 'static {
-    Box::new(move |p, vec| Box::pin(f(p, vec)))
+impl Default for Pagination {
+    fn default() -> Self {
+        Self(5, 0)
+    }
 }
+
+pub type VecOfMaps = Vec<BTreeMap<String, Value>>;
+pub type Count = String;
 
 #[component]
 fn TablesList(entity_name: String) -> impl IntoView {
-    let (pagination, set_pagination) = create_signal::<Pagination>(Pagination(5, 0));
+    let (pagination, set_pagination) = create_signal::<Pagination>(Pagination::default());
     let (maps, set_maps) = create_signal::<VecOfMaps>(vec![]);
     let (count, set_count) = create_signal::<Count>("0".to_string());
+    let (search, set_search) = create_signal::<String>("".to_string());
+    let (prev_search, set_prev_search) = create_signal::<String>("".to_string());
 
-    let fetcher = match entity_name.as_str() {
-        "users" => force_boxed(|p, maps| get_users(p, maps, None)),
-        "admins" => force_boxed(|p, maps| get_users(p, maps, Some(2))),
-        "moderators" => force_boxed(|p, maps| get_users(p, maps, Some(1))),
-        "basics" => force_boxed(|p, maps| get_users(p, maps, Some(0))),
-        "notes" => force_boxed(get_notes),
+    let fetcher_for_users = move |role| create_local_resource(
+        pagination,
+        move |pagination| {
+            let previous_search = search.get();
+
+            if prev_search.get_untracked() == search.get_untracked() {
+                // if search field entry was not changed and fetcher was called due to scrolling then we keep using maps that was previously obtained and conitinue to filling it with new entities
+                let maps = maps.get_untracked();
+                get_users(pagination, maps, role, search.get_untracked())
+            } else {
+                // if search field entry changed then we clearing maps and filling it from scratch with newly requested entities
+                let x = get_users(pagination, vec![], role, search.get_untracked());
+                set_prev_search(previous_search);
+                x
+            }
+        }
+    );
+
+    let resource = match entity_name.as_str() {
+        "users"      => fetcher_for_users(None),
+        "basics"     => fetcher_for_users(Some(0)),
+        "moderators" => fetcher_for_users(Some(1)),
+        "admins"     => fetcher_for_users(Some(2)),
+        "notes"      => create_local_resource(pagination, move |p| get_notes(p, maps.get_untracked())),
         other => panic!("unexpected entity: {}", other)
     };
-
-    let resource = create_local_resource(
-        pagination,
-        move |p| fetcher(p, maps.get_untracked())
-    );
 
     let table_ref = leptos::create_node_ref::<Div>();
 
     let add_items = move || match leptos::window().inner_height() {
         Err(_err) => error!("there was no inner height in window"),
         Ok(inner_height) => match table_ref.get_untracked() {
-            None => info!("there was no element"),
+            None => log::info!("there was no element"),
             Some(element) => {
                 let dom_rect = element.get_bounding_client_rect();
                 let bottom = dom_rect.bottom();
@@ -205,23 +219,24 @@ fn TablesList(entity_name: String) -> impl IntoView {
                 let Pagination(limit, offset) = pagination.get_untracked();
                 let count_as_number = count().parse::<usize>().unwrap();
 
-                // info!("inner height is: {:?} and bottom is: {}, remaingin is {}", inner_height, bottom, remaining);
-                if remaining && offset < count_as_number {
+                if remaining && offset < count_as_number && search.get_untracked() == prev_search.get_untracked() {
                     set_pagination(Pagination(limit, offset + 5))
                 }
             }
         }
     };
 
-    window_event_listener(scroll, move |_ev| add_items());
+    let handle = window_event_listener(scroll, move |_ev| add_items());
+    on_cleanup(move || handle.remove());
 
+    // triggers initial obtaining of items
     create_effect(move |_| match resource.get() {
         None => {
-            todo!()
+            // todo!()
         },
         Some(data) => match data {
             Err(_err) => {
-                todo!()
+                // todo!()
             },
             Ok((new_vec_of_maps, new_count)) => {
                 set_maps(new_vec_of_maps);
@@ -233,15 +248,47 @@ fn TablesList(entity_name: String) -> impl IntoView {
     });
 
     view!{
-        <SearchBar />
+        <SearchBar set_search set_pagination />
 
         <div class="tables-list" node_ref=table_ref>
+
+            <EntityCreationTable columns=BTreeSet::from([
+                "Lorem".to_string(),
+                "Ipsum".to_string(),
+                "Dolor".to_string(),
+                "Set".to_string(),
+                "Amet".to_string(),
+            ]) />
+
             {
                 move || maps()
                     .into_iter()
-                    .map(|n| view! {<TableFromMap map=n.clone() />})
+                    .map(|n| view! { <TableFromMap map=n.clone() /> })
                     .collect_view()
             }
+        </div>
+    }
+}
+
+#[component]
+fn EntityCreationTable(columns: std::collections::BTreeSet<String>) -> impl IntoView {
+    view! {
+        <div>
+            <table>
+                {
+                    columns
+                        .into_iter()
+                        .map(|column| view! { 
+                            <tr>
+                                <th>{column}</th>
+                                <td>
+                                    <input value="" />
+                                </td>
+                            </tr>
+                         })
+                        .collect_view()
+                }
+            </table>
         </div>
     }
 }
@@ -254,19 +301,15 @@ fn ImgsList() -> impl IntoView {
 
     view! {
         {
-            move || {
-                match resource.get() {
-                    None => view! { <p>"Loading..."</p> }.into_view(),
-                    Some(data) => {
-                        match data {
-                            Err(_err) => view! { <p>"Err Loading..."</p> }.into_view(),
-                            Ok(val) => val
-                                .into_iter()
-                                .map(|n| view! {<Img url=n />})
-                                .collect_view()
-                        }
-                    },
-                }
+            move || match resource.get() {
+                None => view! { <p>"Loading..."</p> }.into_view(),
+                Some(data) => match data {
+                    Err(_err) => view! { <p>"Err Loading..."</p> }.into_view(),
+                    Ok(val) => val
+                        .into_iter()
+                        .map(|n| view! {<Img url=n />})
+                        .collect_view()
+                },
             }
         }
     }
@@ -281,33 +324,40 @@ fn Img(url: String) -> impl IntoView {
 #[component]
 fn TableFromMap(map: BTreeMap<String, Value>) -> impl IntoView {
     view! {
-        <table>
-            {
-                map.iter().map(|(key, val)| {
-                    let val_string = match val {
-                        Value::Number(val) => val.to_string(),
-                        Value::String(val) => val.to_string(),
-                        Value::Bool(val) => val.to_string(),
-                        Value::Array(_val) => "it was array".to_string(),
-                        Value::Object(_val) => "it was object".to_string(),
-                        Value::Null => "it was null".to_string(),
-                    };
+        <div class="item-table-wrapper">
+            <table>
+                {
+                    map.iter().map(|(key, val)| {
+                        let val_string = match val {
+                            Value::Number(val) => val.to_string(),
+                            Value::String(val) => val.to_string(),
+                            Value::Bool(val) => val.to_string(),
+                            Value::Array(_val) => "it was array".to_string(),
+                            Value::Object(_val) => "it was object".to_string(),
+                            Value::Null => "it was null".to_string(),
+                        };
 
-                    view! {
-                        <tr>
-                            <th>{key}</th>
-                            <td>{val_string}</td>
-                        </tr>
-                    }
-                }).collect::<Vec<_>>()
-            }
-        </table>
+                        view! {
+                            <tr>
+                                <th>{key}</th>
+                                <td>
+                                    <input type="text" value={val_string} />
+                                </td>
+                            </tr>
+                        }
+                    }).collect::<Vec<_>>()
+                }
+            </table>
+
+            <input type="button" value="update"/>
+            <input type="button" value="delete"/>
+        </div>
     }
 }
 
 
 #[component]
-fn SearchBar() -> impl IntoView {
+fn SearchBar(set_search: WriteSignal<String>, set_pagination: WriteSignal<Pagination>) -> impl IntoView {
     let params = use_params_map();
     let get_entity = move || {
         let enitity_name = params.with(|params| params
@@ -321,6 +371,10 @@ fn SearchBar() -> impl IntoView {
     view! {
         <div class="searchbar-wrapper">
         <input 
+            on:input=move |ev| {
+                set_pagination(Pagination::default());
+                set_search(event_target_value(&ev))
+            }
             type="text" 
             class="searchbar"
             placeholder={get_entity}
